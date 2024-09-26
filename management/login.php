3e90bfd49e751571379608351bb0208c9a27fb36
sAkatsuka <ak5963tvz@yahoo.co.jp>
2024-09-26 19:14:37 +0900

    Update login.php
    表示ボタンの追加

--- management/login.php
+++ management/login.php
@@ -1,10 +1,9 @@
 <?php
 session_start(); 
 
-
 // データベース接続設定
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=work;charset=utf8', 'root', '');
+    $pdo = new PDO('mysql:host=mysql310.phy.lolipop.lan;dbname=LAA1621131-works;charset=utf8', 'LAA1621131', '3776jin');
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo '<p class="center">データベース接続エラー: ' . htmlspecialchars($e->getMessage()) . '</p>';
@@ -44,13 +43,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 require 'header.php';
 require 'nav.php'; 
 
-
 // ログイン画面の表示
 if (!isset($_SESSION['users'])) {
     echo '<form action="login.php" method="post">';
     echo '<div class="center">';
     echo '<p class="login">ログイン名　<input type="text" name="login" required></p>';
-    echo '<p class="login">パスワード　<input type="password" name="password" required></p>';
+    echo '<p class="login">パスワード　<input type="password" name="password" required id="password"> <button type="button" id="togglePassword">表示</button></p>';
     echo '<p class="login"><button type="submit">ログイン</button></p>';
     echo '</div>';
     echo '</form>';
@@ -58,3 +56,12 @@ if (!isset($_SESSION['users'])) {
 
 require 'footer.php'; 
 ?>
+
+<script>
+document.getElementById('togglePassword').addEventListener('click', function () {
+    const passwordField = document.getElementById('password');
+    const buttonText = this.innerText === '表示' ? '非表示' : '表示';
+    this.innerText = buttonText;
+    passwordField.type = passwordField.type === 'password' ? 'text' : 'password';
+});
+</script>
